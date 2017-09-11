c55acfea8c571c10583b65cb5171e073be6b8ab0
Ababii Alex (Maki) <work.maki.5@gmail.com>
2017-09-11 16:01:46 +0300

    users page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def update
+
+    mes = current_user.update_attributes(permitted_params) ? "updated" : book.errors.full_messages
+
+    respond_to do |format|
+      format.html {redirect_to user_path(current_user), notice: mes}
+    end
+  end
+
+  def permitted_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+end
